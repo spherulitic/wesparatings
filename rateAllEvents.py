26c58843b1f1c5ce1b734955494ad590159a8187
yongjianrong <yongjianrong@yahoo.com.sg>
2016-12-24 01:13:38 +0800

    Updated wesparatinglngs , latest ratings
    Latest version of library. Uncomment the line to revert to square root model.
    Latest ratings generated using linear model. 
    Temporary fix of +70 dev after each tourney

--- rateAllEvents.py
+++ rateAllEvents.py
@@ -3,18 +3,29 @@
 import csv
 from wesparatinglib import *
 import os
+import sys 
 
-RATNAME = "rating.dat"
-rootDir = "../tournaments"    # Relative path -- this is where I unzipped the tournaments into
-for root, dirs, files in os.walk(rootDir):
-  for filename in [ y for y in files if 'tou' in y]:  # only want .tou files
-    # do stuff here 
-    TOUNAME = filename
-    tournament = Tournament(RATNAME, TOUNAME)
-    print tournament.getName()
-    print tournament.getDate()
-    tournament.calcRatings()
-    RATNAME = filename.replace('.tou', '.dat')
-    with open(RATNAME, 'w') as f:
-      tournament.outputResults(f)
+RATNAME = "betatrunc.dat"
+TOUIDX = "tou_idx.csv"
+#rootDir = "../Tournaments/"    # Relative path -- this is where I unzipped the tournaments into
+ #for filename in [ y for y in files if '.tou' in y]:  # only want .tou files
+with open(TOUIDX,'r') as f:
+	for row in f.readlines():
+  # do stuff here 
+		LONGROW = row.split(",")
+		TOUPATH = "../" + row[6:9] + "/" + LONGROW[2] + "/"
+		#TOUNAME = str(LONGROW[3]) + ".tou"
+		TOUNAME = os.path.join(row[6:10], LONGROW[1], LONGROW[3]) + ".tou"
+		print TOUNAME #debug
+		tournament = Tournament(RATNAME, TOUNAME)
+		print tournament.getName()
+		print tournament.getDate()
+		tournament.calcRatings() #calls up 'calcNewRatingBySpread'
+		RATNAME = TOUNAME.replace('.tou', '.RT2') #replace old rating file for new iteration
+		print RATNAME + "\n"
+		with open(RATNAME, 'w') as g:
+			tournament.outputRatfile(g)
+			ST2FILE = TOUNAME.replace('.tou', '.ST2')
+			with open(ST2FILE, 'w') as g:
+				tournament.outputResults(g)
 

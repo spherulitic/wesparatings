7a1523f671c2d36fcdd51fa019a16d351be93fc8
spherulitic <chris.lipe@gmail.com>
2017-01-04 22:12:26 -0600

    edit exception handling to rateAllEvents Will now continue when a tournament throws an error   and print to the console

--- rateAllEvents.py
+++ rateAllEvents.py
@@ -10,22 +10,28 @@ TOUIDX = "tou_idx.csv"
 #rootDir = "../Tournaments/"    # Relative path -- this is where I unzipped the tournaments into
  #for filename in [ y for y in files if '.tou' in y]:  # only want .tou files
 with open(TOUIDX,'r') as f:
-	for row in f.readlines():
-  # do stuff here 
-		LONGROW = row.split(",")
-		TOUPATH = "../" + row[6:9] + "/" + LONGROW[2] + "/"
-		#TOUNAME = str(LONGROW[3]) + ".tou"
-		TOUNAME = os.path.join(row[6:10], LONGROW[1], LONGROW[3]) + ".tou"
-		print TOUNAME #debug
-		tournament = Tournament(RATNAME, TOUNAME)
-		print tournament.getName()
-		print tournament.getDate()
-		tournament.calcRatings() #calls up 'calcNewRatingBySpread'
-		RATNAME = TOUNAME.replace('.tou', '.RT2') #replace old rating file for new iteration
-		print RATNAME + "\n"
-		with open(RATNAME, 'w') as g:
-			tournament.outputRatfile(g)
-			ST2FILE = TOUNAME.replace('.tou', '.ST2')
-			with open(ST2FILE, 'w') as g:
-				tournament.outputResults(g)
+  for row in f.readlines():
+    try:
+    # do stuff here 
+      LONGROW = row.split(",")
+      TOUPATH = "../" + row[6:9] + "/" + LONGROW[2] + "/"
+      #TOUNAME = str(LONGROW[3]) + ".tou"
+      TOUNAME = os.path.join(row[6:10], LONGROW[1], LONGROW[3]) + ".tou"
+      print TOUNAME #debug
+      tournament = Tournament(RATNAME, TOUNAME)
+      print tournament.getName()
+      print tournament.getDate()
+      tournament.calcRatings() #calls up 'calcNewRatingBySpread'
+      RATNAME = TOUNAME.replace('.tou', '.RT2') #replace old rating file for new iteration
+      print RATNAME + "\n"
+      with open(RATNAME, 'w') as g:
+        tournament.outputRatfile(g)
+        ST2FILE = TOUNAME.replace('.tou', '.ST2')
+        with open(ST2FILE, 'w') as g:
+          tournament.outputResults(g)
+    except Exception as ex:
+      template = "An exception of type {0} occured. TOUFILE: {1}"
+      message = template.format(type(ex).__name__, TOUNAME)
+      print message # will print to screen for now; can redirect this to a file
+
 
